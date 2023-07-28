00008abfc09fd3fdba3ca835b9aa4523dd9fbd52
Roman <sheikhroman042@gmail.com>
2023-07-28 12:33:28 +0600

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Navber from './components/Navbar'
+import { Providers } from './components/Providers'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,8 +17,13 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.className} bg-white text-black dark:bg-gray-900 dark:selection:bg-gray-900 dark:text-white h-full selection:bg-gray-50`}>
+        <Providers>
+          <Navber />
+          <main className='max-w-3xl mx-auto px-4 sm:px-6 lg:px-8'>{children}</main>
+        </Providers>
+      </body>
     </html>
   )
 }
---
+++ sanity/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'ukxz3rft',
+    dataset: 'production'
+  }
+})
